ed4f5d8701b89e135d9805cfcde97473f2c6bc53
rrana <rana@newscred.com>
2017-09-18 11:18:27 +0600

    Add a script that retrives public IP address of an interface by connecting to an IP in the internet

---
+++ misc-tools/get-public-ip.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# please upvote this answer if you're using this script
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+print(s.getsockname()[0])
+s.close()
